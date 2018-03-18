3327181e4d1ad820692dac4fed8cb51c9d6777a3
davide <davec82@gmail.com>
2018-03-18 18:25:45 +0100

    fix elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule LoginPhoneCall.Mixfile do
     [
       app: :stop_unwanted_calls,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.4",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       elixirc_paths: elixirc_paths(Mix.env()),
